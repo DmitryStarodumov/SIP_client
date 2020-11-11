da517c99f4a3195fd9d8e47d146bf79a11f06337
Dmitry Starodumov <starodumov.dmitriy@mail.ru>
2020-11-11 15:27:26 +0300

    Add files via upload

--- MyForm.cpp
+++ MyForm.cpp
@@ -29,7 +29,9 @@ struct app_config {
     char* sip_password = "2";
     char* sip_port = "5060";
     char* phone_number;
+    int repetition_limit = 3;
 } app_cfg;
+
 static void app_exit()
 {   // check if player/recorder is active and stop them
     if (play_id != -1) pjsua_player_destroy(play_id);
@@ -39,47 +41,6 @@ static void app_exit()
     pjsua_destroy();
     exit(0);
 }
-/* Callback called by the library upon receiving incoming call */
-static void on_incoming_call(pjsua_acc_id acc_id, pjsua_call_id call_id, pjsip_rx_data* rdata)
-{
-    pjsua_call_info ci;
-    pjsua_call_get_info(call_id, &ci);
-    /* Automatically answer incoming calls with 200/OK */
-    pjsua_call_answer(call_id, 200, NULL, NULL);
-}
-/* Callback called by the library when call's media state has changed */
-static void on_call_media_state(pjsua_call_id call_id)
-{
-    pjsua_call_info ci;
-    pjsua_call_get_info(call_id, &ci);
-
-    if (ci.media_status == PJSUA_CALL_MEDIA_ACTIVE)
-    {
-        pjsua_conf_connect(ci.conf_slot, 0);
-        pjsua_conf_connect(0, ci.conf_slot);
-    }
-}
-// handler for call-state-change-events
-static void on_call_state(pjsua_call_id call_id, pjsip_event* e)
-{    // get call infos
-    pjsua_call_info ci;
-    pjsua_call_get_info(call_id, &ci);
-    // prevent warning about unused argument e
-    PJ_UNUSED_ARG(e);
-    // check call state
-    if (ci.state == PJSIP_INV_STATE_CONFIRMED)
-    {    // ensure that message is played from start
-        if (play_id != PJSUA_INVALID_ID)
-        {
-            pjmedia_wav_player_port_set_pos(play_port, 0);
-        }
-    }
-    if (ci.state == PJSIP_INV_STATE_DISCONNECTED)
-    {   // exit app if call is finished/disconnected
-        app_exit();
-    }
-}
-/* Display error and exit application */
 static void error_exit(const char* title, pj_status_t status)
 {
     if (play_id != -1) pjsua_player_destroy(play_id);
@@ -89,30 +50,67 @@ static void error_exit(const char* title, pj_status_t status)
     pjsua_destroy();
     exit(1);
 }
+// handler for call-media-state-change-events
+static void on_call_media_state(pjsua_call_id call_id)
+{	// get call infos
+	pjsua_call_info ci;
+	pjsua_call_get_info(call_id, &ci);
+	pj_status_t status;
+    for (unsigned i = 0; i < ci.media_cnt; ++i) {
+        if (ci.media[i].type == PJMEDIA_TYPE_AUDIO) {
+            switch (ci.media[i].status) {
+            case PJSUA_CALL_MEDIA_ACTIVE:
+                pjsua_conf_connect(ci.media[i].stream.aud.conf_slot, 0);
+                pjsua_conf_connect(0, ci.media[i].stream.aud.conf_slot);
+                break;
+            default:
+                break;
+            }
+        }
+    }
+}
+// handler for call-state-change-events
+static void on_call_state(pjsua_call_id call_id, pjsip_event* e)
+{	// get call infos
+	pjsua_call_info ci;
+	pjsua_call_get_info(call_id, &ci);
+	// prevent warning about unused argument e
+	PJ_UNUSED_ARG(e);
+	// check call state
+	if (ci.state == PJSIP_INV_STATE_DISCONNECTED)
+	{	// exit app if call is finished/disconnected
+		app_exit();
+	}
+}
+    // incoming call notice
+static void on_incoming_call(pjsua_acc_id acc_id, pjsua_call_id call_id, pjsip_rx_data* rdata) {
+    PJ_UNUSED_ARG(acc_id);
+    PJ_UNUSED_ARG(rdata);
+    pj_status_t status;
+    pjsua_call_info ci;
+    char info[80];
+    pjsua_call_get_info(call_id, &ci);
+    status = pjsua_call_answer(call_id, 200, NULL, NULL);
+    snprintf(info, sizeof(info), "Incoming call from %.*s", (int)ci.remote_info.slen, ci.remote_info.ptr);
+}
 void SIPclient::MyForm::CallBtn_Click(System::Object^ sender, System::EventArgs^ e) {
     label4->Text += "Starting call ... ";
     // build target sip-url
-    //sprintf(sip_target_url, "sip:%s@%s", app_cfg.phone_number, app_cfg.sip_domain);
-    //sprintf(sip_target_url, "sip:%s@192.168.172.1", app_cfg.phone_number, app_cfg.sip_domain);
     char sip_target_url[40];
-    sprintf(sip_target_url, "sip:%s@%s", app_cfg.phone_number, app_cfg.sip_domain);//;transport=tcp
+    sprintf(sip_target_url, "sip:%s@%s;transport=tcp", app_cfg.phone_number, app_cfg.sip_domain);//;transport=tcp
     // start call with sip-url
     pj_str_t uri = pj_str(sip_target_url);
     status = pjsua_call_make_call(acc_id, &uri, 0, NULL, NULL, NULL);
     system("pause");
     if (status != PJ_SUCCESS) error_exit("Error making call", status);
-    if (status == PJ_SUCCESS) label4->Text += "Done.\n";
 }
 void SIPclient::MyForm::DeclineBtn_Click(System::Object^ sender, System::EventArgs^ e) {
     pjsua_call_hangup_all();
-    if (acc_id != -1) {pjsua_acc_del(acc_id);}
-    pjsua_destroy();
 }
 void SIPclient::MyForm::ServerIPTxt_TextChanged(System::Object^ sender, System::EventArgs^ e) {
     app_cfg.sip_domain = (char*)(System::Runtime::InteropServices::Marshal::StringToHGlobalAnsi(serverIPTxt->Text)).ToPointer();
 }
 void SIPclient::MyForm::PortTxt_TextChanged(System::Object^ sender, System::EventArgs^ e) {
-	//port = System::Convert::ToInt32(portTxt->Text);
     app_cfg.sip_port = (char*)(System::Runtime::InteropServices::Marshal::StringToHGlobalAnsi(portTxt->Text)).ToPointer();
 }
 void SIPclient::MyForm::userTxt_TextChanged(System::Object^ sender, System::EventArgs^ e) {
@@ -132,12 +130,9 @@ void SIPclient::MyForm::RegisterBtn_Click(System::Object^ sender, System::EventA
     // build sip-user-url
     char sip_user_url[40];
     sprintf(sip_user_url, "sip:%s@%s", app_cfg.sip_user, app_cfg.sip_domain);
-    //sprintf(sip_user_url, "sip:3460210@192.168.39.18:5060");
-   
     // build sip-provder-url
     char sip_provider_url[40];
-    sprintf(sip_provider_url, "sip:%s", app_cfg.sip_domain);//;transport=tcp
-    //sprintf(sip_provider_url, "sip:192.168.39.18:5060;transport=tcp");
+    sprintf(sip_provider_url, "sip:%s;transport=tcp", app_cfg.sip_domain);//;transport=tcp
     // create and define account
     cfg.id = pj_str(sip_user_url);
     cfg.reg_uri = pj_str(sip_provider_url);
@@ -150,13 +145,10 @@ void SIPclient::MyForm::RegisterBtn_Click(System::Object^ sender, System::EventA
     // add account
     status = pjsua_acc_add(&cfg, PJ_TRUE, &acc_id);
     if (status != PJ_SUCCESS) error_exit("Error adding account", status);
-    status = pjsua_start();
-    if (status != PJ_SUCCESS) error_exit("Error starting pjsua", status);
     if (status == PJ_SUCCESS) label4->Text += "Done.\n";
 }
 void SIPclient::MyForm::start_sip_stack()
 {
-    pjsua_transport_id tcp_tp_id;
     status = pjsua_create();
     if (status != PJ_SUCCESS)
         error_exit("Error in pjsua_create()", status);
@@ -165,7 +157,7 @@ void SIPclient::MyForm::start_sip_stack()
     // enable just 1 simultaneous call 
     cfg.max_calls = 1;
     // callback configuration		
-    cfg.cb.on_incoming_call = &on_incoming_call;
+    //fg.cb.on_incoming_call = &on_incoming_call;
     cfg.cb.on_call_media_state = &on_call_media_state;
     cfg.cb.on_call_state = &on_call_state;
     // initialize pjsua 
@@ -175,42 +167,10 @@ void SIPclient::MyForm::start_sip_stack()
     pjsua_transport_config tcfg;
     pjsua_transport_config_default(&tcfg);
     tcfg.port = 5060;
-    status = pjsua_transport_create(PJSIP_TRANSPORT_UDP, &tcfg, &tcp_tp_id);
+    status = pjsua_transport_create(PJSIP_TRANSPORT_TCP, &tcfg, NULL);
     if (status != PJ_SUCCESS) error_exit("Error creating transport", status);
+    status = pjsua_start();
+    if (status != PJ_SUCCESS) error_exit("Error starting pjsua", status);
+
 }
 void SIPclient::MyForm::stop_sip_stack() { app_exit(); }
-
-/*
-void SIPclient::MyForm::AdjustTranVolume(pjsua_call_id call_id, float level)
-{
-    pjsua_call_info ci;
-    pjsua_call_get_info(call_id, &ci);
-#ifdef DEBUG
-    unsigned tx_level = 0;
-    unsigned rx_level = 0;
-    status = pjsua_conf_get_signal_level(ci.conf_slot, &tx_level, &rx_level);
-    if (status != PJ_SUCCESS)
-        error_exit("Error get signal level", status);
-#endif
-    status = pjsua_conf_adjust_tx_level(ci.conf_slot, level);
-    if (status != PJ_SUCCESS)
-        error_exit("Error adjust tx level", status);
-}
-
-void SIPclient::MyForm::AdjustRecvVolume(pjsua_call_id call_id, float level)
-{
-    pjsua_call_info ci;
-    pjsua_call_get_info(call_id, &ci);
-
-#ifdef DEBUG
-    unsigned tx_level = 0;
-    unsigned rx_level = 0;
-    status = pjsua_conf_get_signal_level(ci.conf_slot, &tx_level, &rx_level);
-    if (status != PJ_SUCCESS)
-        error_exit("Error get signal level", status);
-#endif
-    status = pjsua_conf_adjust_rx_level(ci.conf_slot, level);
-    if (status != PJ_SUCCESS)
-        error_exit("Error adjust rx level", status);
-}
-*/
